5eeba0bd0d400ae574ae96e8590fb511624afcf5
Lidia <luwineza64@gmail.com>
2025-03-11 15:19:28 +0200

    Add files via upload
    beginner-friendly project ideas:Personalized Greeting App 👋

---
+++ week1/greeting.py
@@ -0,0 +1,4 @@
+User_Name = input(' Enter Your Names : ')
+Fav_Coloer = input(' Enter Your Favorite color : ')
+
+print('Hello👋', User_Name , 'your favorite color is :' ,Fav_Coloer,'The',Fav_Coloer,'is awesome' )
